ee1cb29d0369f17cf545bd5b9cce83bedf784279
csterpis@redhat.com <csterpis@redhat.com>
2015-06-22 14:35:46 +0000

    update file /www/js/login.js

--- www/js/login.js
+++ www/js/login.js
@@ -1,16 +1,23 @@
 function doLogin() {
 	var username = document.getElementById('username').value;
     document.getElementById('loggedInResponse').innerHTML = "<p>Logging you in " + username + "...</p>";
-    window.location.replace('#home');
-/*    $fh.cloud(
+    //window.location.replace('#home');
+    $fh.cloud(
         {
-            path: 'hello',
-            data: {
-                hello: document.getElementById('loggedInInfo').value
-            }
+            path: 'cloud/login?username=' + username,
+            // data: {
+            //     hello: document.getElementById('loggedInInfo').value
+            // }
         },
         function (res) {
-            document.getElementById('loggedInInfo').innerHTML = "<p>" + res.msg + "</p>";
+            
+            if (res.msg = ''){
+              document.getElementById('loggedInInfo').innerHTML = "<p>No user found!</p>";
+            } else{
+              document.getElementById('loggedInInfo').innerHTML = "<p>" + res.msg.username+  " user found!</p>";
+            
+            }
+            
         },
         function (code, errorprops, params) {
             alert('An error occured: ' + code + ' : ' + errorprops);
