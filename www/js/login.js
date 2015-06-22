2585a0c01f2b8ba0a40b4d0ce8aee301adcba045
Costas Sterpis <costas.sterpis@gmail.com>
2015-06-22 17:45:03 +0100

    ADD: accept-type CHG: json manipulation (should work after back-end fix)

--- www/js/login.js
+++ www/js/login.js
@@ -1,26 +1,31 @@
 function doLogin() {
-	var username = document.getElementById('username').value;
+    var username = document.getElementById('username').value;
     document.getElementById('loggedInResponse').innerHTML = "<p>Logging you in " + username + "...</p>";
     //window.location.replace('#home');
     $fh.cloud(
         {
-            path: '/cloud/login?username=' + username,
-            // data: {
-            //     hello: document.getElementById('loggedInInfo').value
-            // }
+            "path": '/cloud/login?username=' + username,
+            "contentType": "application/json",
+            "method": "GET"
         },
         function (res) {
-            
-            if (res.msg = '[]'){
-              document.getElementById('loggedInResponse').innerHTML = "<p>No user found!</p>";
-            } else{
-              document.getElementById('loggedInResponse').innerHTML = "<p>" + res.msg.username+  " user found!</p>";
-            
+            //var response = JSON.parse(res);
+
+            //alert("JSON.stringify(res) + " + response);
+            //alert(response);
+            //alert(res);
+            if (res == '[]') {
+                document.getElementById('loggedInResponse').innerHTML = "<p>No user found!</p>";
+            }else {
+                document.getElementById('loggedInResponse').innerHTML = "<p>" + res.username + " user found!</p>";
+                //window.location.replace('#home');
             }
-            
+
         },
-        function (code, errorprops, params) {
-            alert('An error occured: ' + code + ' : ' + errorprops);
+        function(msg,err) {
+            // An error occured during the cloud call. Alert some debugging information
+            alert('Cloud call failed with error message:' + msg + '. Error properties:' + JSON.stringify(err));
         }
-    );*/
+    );
+
 };
