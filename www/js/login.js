5c6a0fd22df0f674fe0d89b69e5e90b8d10999a3
Costas Sterpis <costas.sterpis@gmail.com>
2015-06-24 13:49:05 +0100

    add: hidden field having user id, chg:user profile page changes

--- www/js/login.js
+++ www/js/login.js
@@ -12,21 +12,26 @@ function doLogin() {
             if (res == '') { //res[0] = undefined
                 document.getElementById('loggedInResponse').innerHTML = "<p>No user found!</p>";
             } else {
+                var id = res[0].id;
                 var username = res[0].username;
                 var firstName = res[0].firstname;
                 var address = res[0].address;
                 var surname = res[0].surname;
+                var email = res[0].email;
+
                 document.getElementById('loggedInUser').innerHTML = firstName + " " + surname;
                 document.getElementById('loggedInResponse').innerHTML = "<p></p>";
                 document.getElementById('firstname').value = firstName;
                 document.getElementById('surname').value = surname;
                 document.getElementById('address').value = address;
-
+                document.getElementById('email').value = email;
+                document.getElementById('loggedUsername').innerHTML = username;
+                document.getElementById('loggedInUserId').value = id;
                 window.location.replace('#home');
             }
 
         },
-        function (msg, err) {
+        function(msg,err) {
             // An error occured during the cloud call. Alert some debugging information
             alert('Cloud call failed with error message:' + msg + '. Error properties:' + JSON.stringify(err));
         }
