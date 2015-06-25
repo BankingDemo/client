1cf7e6fa62d05f2539e2d5e48514f586a790356d
Costas Sterpis <costas.sterpis@gmail.com>
2015-06-25 12:38:43 +0100

    working impl of transfer money

--- www/js/transfer.js
+++ www/js/transfer.js
@@ -1,6 +1,31 @@
-function doTransfer(){
+function doTransfer() {
 
     var fromId = document.getElementById('loggedInUserId').value;
-    alert("fromId:" + fromId );
+    var toId = document.getElementById('recipient-account').value;
 
+    var amount = +document.getElementById('how-much').value + +(document.getElementById('how-much-cents').value / 100);
+
+    var payee = document.getElementById('recipient-account-boilerplate').value + toId;
+    alert("fromId:" + fromId + " toId:" + toId + " amount:" + amount + " payee:" + payee);
+
+    $fh.cloud(
+        {
+            "path": '/cloud/transferMoney?fromId=' + fromId + "&toId=" + toId + "&amount=" + amount + "&payee=" + payee,
+            "contentType": "application/json",
+            "method": "GET"
+        },
+        function (res) {
+            if (res == '-1') {
+                document.getElementById('transferInfo').innerHTML = "<p>The transfer could not be completed.</p>";
+            } else {
+                document.getElementById('transferInfo').innerHTML = "<p>The transfer was completed successfully.</p>";
+                window.location.replace('#home');
+            }
+
+        },
+        function (msg, err) {
+            // An error occured during the cloud call. Alert some debugging information
+            alert('Cloud call failed with error message:' + msg + '. Error properties:' + JSON.stringify(err));
+        }
+    );
 }
