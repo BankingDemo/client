c97c084e6890d2c8c493bd6f0339e687c9efee9a
Costas Sterpis <costas.sterpis@gmail.com>
2015-06-25 18:44:58 +0100

    working impl of getTransactions -- home page

--- www/js/login.js
+++ www/js/login.js
@@ -1,7 +1,6 @@
 function doLogin() {
     var username = document.getElementById('username').value;
     document.getElementById('loggedInResponse').innerHTML = "<p>Logging you in " + username + "...</p>";
-    //window.location.replace('#home');
     $fh.cloud(
         {
             "path": '/cloud/login?username=' + username,
@@ -28,15 +27,45 @@ function doLogin() {
                 document.getElementById('amount').innerHTML = amount;
                 document.getElementById('loggedInUsername').value = username;
                 document.getElementById('loggedInUserId').value = id;
-                window.location.replace('#home');
+
+                //getTransactions for loggedIn user
+                $fh.cloud(
+                    {
+                        "path": '/cloud/getTransactions?id=' + id,
+                        "contentType": "application/json",
+                        "method": "GET"
+                    },
+                    function (getTxRes) {
+                        var tbody = "";
+                        if (getTxRes != '-1') {
+                            $.each(getTxRes, function (idx, transaction) {
+                                tbody += "<tr>" +
+                                    "<td>" + transaction.fromid + "</td>" +
+                                    "<td>" + transaction.toId + "</td>" +
+                                    "<td>" + transaction.payee + "</td>" +
+                                    "<td>" + transaction.amount + "</td>" +
+                                    "<td>" + transaction.details + "</td>" +
+                                    "<td>" + transaction.txdate + "</td>" +
+                                    " </tr> \n";
+                            });
+                            document.getElementById('transactionsList').innerHTML = tbody;
+                            window.location.replace('#home');
+                        }
+                    },
+                    function (getTxMsg, getTxErr) {
+                        // An error occured during the cloud call. Alert some debugging information
+                        alert('Cloud call failed with error message:' + getTxMsg + '. Error properties:' + JSON.stringify(getTxErr));
+                    }
+                );
+
             }
 
         },
-        function(msg,err) {
+        function (msg, err) {
             // An error occured during the cloud call. Alert some debugging information
             alert('Cloud call failed with error message:' + msg + '. Error properties:' + JSON.stringify(err));
         }
-        
-    );
+    )
+
 
 };
