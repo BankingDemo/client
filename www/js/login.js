a647b51a9795b9b93172b43306cd9eaeb681b18a
Nabeel Saad <nsaad@redhat.com>
2015-06-26 17:28:40 +0100

    one more alternating color fix, added icons, added refresh functionality for transactions

--- www/js/login.js
+++ www/js/login.js
@@ -5,6 +5,79 @@ function fillRepeatedFields(elementClassName, valueToFill) {
      }
 }
 
+function loadTransactions(id) {
+	//getTransactions for loggedIn user
+    $fh.cloud(
+        {
+            "path": '/cloud/getTransactions?id=' + id,
+            "contentType": "application/json",
+            "method": "GET"
+        },
+        function (getTxRes) {
+            var tbody = "";
+            var operation = "";
+
+            if (getTxRes != '-1') {
+                $.each(getTxRes, function (idx, transaction) {
+                	
+                	var decimalAmount = parseFloat(Math.round(transaction.amount * 100) / 100).toFixed(2);
+                
+                    tbody += "<tr>" +
+                    	"<td>" + transaction.txdate + "</td>";
+                        
+                    if (transaction.toId != -1 && transaction.toId != id && transaction.toId != 99 && transaction.fromid != "-1") {
+                    	tbody += 
+                        	"<td> Money sent to account: " + transaction.payee + "</td>" +
+                        	"<td>" + transaction.details + "</td>";
+                    	
+                    	operation = "-";
+                    } else if (transaction.toId != -1 && transaction.toId == id && transaction.toId != 99 && transaction.fromid != "-1") {
+                    	tbody += 
+                        	"<td> Money received from account: " + transaction.payee + "</td>" +
+                        	"<td>" + transaction.details + "</td>";
+                    	
+                    	operation = "+";
+                    } else if (transaction.toId == 99) {
+                    	//Payment to some company should have toId 99
+                    	tbody += 
+                    		"<td>" + transaction.payee + "</td>" +
+                    		"<td> Payment</td>";
+                    	
+                    	operation = "-";
+                    } else {
+                    
+                    	tbody += "<td>" + transaction.payee + "</td>" +
+                        "<td>" + transaction.details + "</td>";
+                    }
+                    
+                    if (transaction.toId == -1) {
+                    	operation = "-";
+                    } else if (transaction.fromid == -1 && transaction.toId == id) {
+                    	operation = "+";
+                    }
+
+                    if (operation == "+") {
+                    	tbody +=
+                    		"<td>&#163; +" + decimalAmount + "</td><td></td>" +
+                            " </tr> \n";
+                    } else {
+                    	tbody +=
+                            "<td></td><td>&#163; -" + decimalAmount + "</td>" +
+                            " </tr> \n";
+                    }
+                    
+                });
+                document.getElementById('transactionsList').innerHTML = tbody;
+                window.location.replace('#home');
+            }
+        },
+        function (getTxMsg, getTxErr) {
+            // An error occured during the cloud call. Alert some debugging information
+            alert('Cloud call failed with error message:' + getTxMsg + '. Error properties:' + JSON.stringify(getTxErr));
+        }
+    );
+}
+
 function doLogin() {
     var username = document.getElementById('username').value;
     document.getElementById('loggedInResponse').innerHTML = "<p>Logging you in " + username + "...</p>";
@@ -38,77 +111,7 @@ function doLogin() {
                 fillRepeatedFields('headerFullname', firstName + " " + surname);
                 fillRepeatedFields('loggedInUserId', id);
                 
-                //getTransactions for loggedIn user
-                $fh.cloud(
-                    {
-                        "path": '/cloud/getTransactions?id=' + id,
-                        "contentType": "application/json",
-                        "method": "GET"
-                    },
-                    function (getTxRes) {
-                        var tbody = "";
-                        var operation = "";
-
-                        if (getTxRes != '-1') {
-                            $.each(getTxRes, function (idx, transaction) {
-                            	
-                            	var decimalAmount = parseFloat(Math.round(transaction.amount * 100) / 100).toFixed(2);
-                            
-                                tbody += "<tr>" +
-                                	"<td>" + transaction.txdate + "</td>";
-                                    
-                                if (transaction.toId != -1 && transaction.toId != id && transaction.toId != 99 && transaction.fromid != "-1") {
-                                	tbody += 
-                                    	"<td> Money sent to account: " + transaction.payee + "</td>" +
-                                    	"<td>" + transaction.details + "</td>";
-                                	
-                                	operation = "-";
-                                } else if (transaction.toId != -1 && transaction.toId == id && transaction.toId != 99 && transaction.fromid != "-1") {
-                                	tbody += 
-                                    	"<td> Money received from account: " + transaction.payee + "</td>" +
-                                    	"<td>" + transaction.details + "</td>";
-                                	
-                                	operation = "+";
-                                } else if (transaction.toId == 99) {
-                                	//Payment to some company should have toId 99
-                                	tbody += 
-                                		"<td>" + transaction.payee + "</td>" +
-                                		"<td> Payment</td>";
-                                	
-                                	operation = "-";
-                                } else {
-                                
-                                	tbody += "<td>" + transaction.payee + "</td>" +
-                                    "<td>" + transaction.details + "</td>";
-                                }
-                                
-                                if (transaction.toId == -1) {
-                                	operation = "-";
-                                } else if (transaction.fromid == -1 && transaction.toId == id) {
-                                	operation = "+";
-                                }
-
-                                if (operation == "+") {
-                                	tbody +=
-                                		"<td>&#163; +" + decimalAmount + "</td><td></td>" +
-                                        " </tr> \n";
-                                } else {
-                                	tbody +=
-                                        "<td></td><td>&#163; -" + decimalAmount + "</td>" +
-                                        " </tr> \n";
-                                }
-                                
-                            });
-                            document.getElementById('transactionsList').innerHTML = tbody;
-                            window.location.replace('#home');
-                        }
-                    },
-                    function (getTxMsg, getTxErr) {
-                        // An error occured during the cloud call. Alert some debugging information
-                        alert('Cloud call failed with error message:' + getTxMsg + '. Error properties:' + JSON.stringify(getTxErr));
-                    }
-                );
-
+                loadTransactions(id);
             }
 
         },
